5e6876f41ed46ebf0ae89596e799bbcd555d2b24
Og Maciel <omaciel@ogmaciel.com>
2015-11-20 16:51:17 -0500

    Use phantomjs by default

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cd /root/robottelo
-
-# Update Robottelo code
-git pull
+# Checkout Robottelo
+git clone -q https://github.com/SatelliteQE/robottelo.git
 
 # Update dependencies, if needed
+cd /root/robottelo
+
 pip install -q --upgrade -r requirements.txt
 pip install -q --upgrade -r requirements-optional.txt
 
@@ -16,5 +16,7 @@ sed -i "s/^hostname.*/hostname=${SERVER_URL}/" robottelo.properties
 sed -i "s/^ssh_key.*/ssh_key=${SSH_KEY}/" robottelo.properties
 sed -i "s/^project.*/project=satellite6/" robottelo.properties
 sed -i 's/^upstream.*/upstream=false/' robottelo.properties
+sed -i "s/^# [robottelo].*/[robottelo]/" robottelo.properties
+sed -i "s/^# webdriver.*/webdriver=phantomjs/" robottelo.properties
 
 nosetests -v ${TESTS}
